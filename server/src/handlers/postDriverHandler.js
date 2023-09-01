c97d677a4f43fe261c8d1049c7624904481dc2d2
danielchavezs <danielechs@hotmail.com>
2023-09-01 11:11:16 -0500

    Two functional handlers added.

---
+++ server/src/handlers/postDriverHandler.js
@@ -0,0 +1,17 @@
+const { createDriver } = require("../controllers/postDriverController");
+
+const postHandler = async (req, res) => {
+    // console.log('Llegó la request hasta el handler')
+    const { forename, surname, description, image, nationality, dob, teams, } = req.body;
+    // console.log(forename, surname, description, image, nationality, dob, teams);
+    // console.log(`ACÁ ESTÁ EL DOB: ${dob} RECIBIDO EN EL HANDLER`)
+    try {
+        const response = await createDriver(forename, surname, description, image, nationality, dob, teams);
+        console.log(response);
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+module.exports = { postHandler };
